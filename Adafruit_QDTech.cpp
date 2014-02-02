6c9e20e49fc0ecfc873873afe7b6d708df55d51d
zigwart <chris@hmu.com>
2014-02-03 10:55:32 +1300

    Create Adafruit_QDTech.cpp

---
+++ Adafruit_QDTech.cpp
@@ -0,0 +1,400 @@
+/***************************************************
+	This is a modification of the Adafruit SPI LCD library,
+	customised for hardware SPI and the QDTech board 
+	using a Samsung S6D02A1 chip.
+
+	Most changes are made to the initialisation routine but
+	non-Arduino code has been removed too.
+
+	The initialisation sequence comes from Henning Karlsen's
+	UTFT library: http://henningkarlsen.com
+
+	Using the hardware SPI pins is highly recommeneded.
+	
+	You will also need the stock "Adafruit_GFX" library.
+	https://github.com/adafruit/Adafruit-GFX-Library
+
+	Gilchrist 30/1/2014
+
+/***************************************************
+  This is a library for the Adafruit 1.8" SPI display.
+  This library works with the Adafruit 1.8" TFT Breakout w/SD card
+  ----> http://www.adafruit.com/products/358
+  as well as Adafruit raw 1.8" TFT display
+  ----> http://www.adafruit.com/products/618
+ 
+  Check out the links above for our tutorials and wiring diagrams
+  These displays use SPI to communicate, 4 or 5 pins are required to
+  interface (RST is optional)
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include "Adafruit_QDTech.h"
+#include <limits.h>
+#include "pins_arduino.h"
+#include "wiring_private.h"
+#include <SPI.h>
+
+inline uint16_t swapcolor(uint16_t x) { 
+  return (x << 11) | (x & 0x07E0) | (x >> 11);
+}
+
+
+// Constructor when using software SPI.  All output pins are configurable.
+Adafruit_QDTech::Adafruit_QDTech(uint8_t cs, uint8_t rs, uint8_t sid,
+ uint8_t sclk, uint8_t rst) : Adafruit_GFX(QDTech_TFTWIDTH, QDTech_TFTHEIGHT)
+{
+  _cs   = cs;
+  _rs   = rs;
+  _sid  = sid;
+  _sclk = sclk;
+  _rst  = rst;
+  hwSPI = false;
+}
+
+
+// Constructor when using hardware SPI.  Faster, but must use SPI pins
+// specific to each board type (e.g. 11,13 for Uno, 51,52 for Mega, etc.)
+Adafruit_QDTech::Adafruit_QDTech(uint8_t cs, uint8_t rs, uint8_t rst) :
+    Adafruit_GFX(QDTech_TFTWIDTH, QDTech_TFTHEIGHT) {
+  _cs   = cs;
+  _rs   = rs;
+  _rst  = rst;
+  hwSPI = true;
+  _sid  = _sclk = 0;
+}
+
+inline void Adafruit_QDTech::spiwrite(uint8_t c) {
+
+  if (hwSPI) {
+    SPDR = c;
+    while(!(SPSR & _BV(SPIF)));
+  } else {
+    // Fast SPI bitbang swiped from LPD8806 library
+    for(uint8_t bit = 0x80; bit; bit >>= 1) {
+      if(c & bit) *dataport |=  datapinmask;
+      else        *dataport &= ~datapinmask;
+      *clkport |=  clkpinmask;
+      *clkport &= ~clkpinmask;
+    }
+  }
+}
+
+
+void Adafruit_QDTech::writecommand(uint8_t c) {
+  *rsport &= ~rspinmask;
+  *csport &= ~cspinmask;
+  spiwrite(c);
+  *csport |= cspinmask;
+}
+
+
+void Adafruit_QDTech::writedata(uint8_t c) {
+  *rsport |=  rspinmask;
+  *csport &= ~cspinmask;
+  spiwrite(c);
+  *csport |= cspinmask;
+} 
+
+
+// Rather than a bazillion writecommand() and writedata() calls, screen
+// initialization commands and arguments are organized in these tables
+// stored in PROGMEM.  The table may look bulky, but that's mostly the
+// formatting -- storage-wise this is hundreds of bytes more compact
+// than the equivalent code.  Companion function follows.
+#define DELAY 0x80
+
+
+static const uint8_t PROGMEM	// Multiple LCD init commands removed
+  QDTech[] = {                  // QDTech support only now
+	29,
+	0xf0,	2,	0x5a, 0x5a,
+	0xfc,	2,	0x5a, 0x5a,
+	0x26,	1,	0x01,
+	0xfa,	15,	0x02, 0x1f,	0x00, 0x10,	0x22, 0x30, 0x38, 0x3A, 0x3A, 0x3A,	0x3A, 0x3A,	0x3d, 0x02, 0x01,
+	0xfb,	15,	0x21, 0x00,	0x02, 0x04,	0x07, 0x0a, 0x0b, 0x0c, 0x0c, 0x16,	0x1e, 0x30,	0x3f, 0x01, 0x02,
+	0xfd,	11,	0x00, 0x00, 0x00, 0x17, 0x10, 0x00, 0x01, 0x01, 0x00, 0x1f, 0x1f,
+	0xf4,	15, 0x00, 0x00, 0x00, 0x00, 0x00, 0x3f, 0x3f, 0x07, 0x00, 0x3C, 0x36, 0x00, 0x3C, 0x36, 0x00,
+	0xf5,	13, 0x00, 0x70, 0x66, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x6d, 0x66, 0x06,
+	0xf6,	11, 0x02, 0x00, 0x3f, 0x00, 0x00, 0x00, 0x02, 0x00, 0x06, 0x01, 0x00,
+	0xf2,	17, 0x00, 0x01, 0x03, 0x08, 0x08, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x04, 0x08, 0x08,
+	0xf8,	1,	0x11,
+	0xf7,	4, 0xc8, 0x20, 0x00, 0x00,
+	0xf3,	2, 0x00, 0x00,
+	0x11,	DELAY, 50,
+	0xf3,	2+DELAY, 0x00, 0x01, 50,
+	0xf3,	2+DELAY, 0x00, 0x03, 50,
+	0xf3,	2+DELAY, 0x00, 0x07, 50,
+	0xf3,	2+DELAY, 0x00, 0x0f, 50,
+	0xf4,	15+DELAY, 0x00, 0x04, 0x00, 0x00, 0x00, 0x3f, 0x3f, 0x07, 0x00, 0x3C, 0x36, 0x00, 0x3C, 0x36, 0x00, 50,
+	0xf3,	2+DELAY, 0x00, 0x1f, 50,
+	0xf3,	2+DELAY, 0x00, 0x7f, 50,
+	0xf3,	2+DELAY, 0x00, 0xff, 50,
+	0xfd,	11, 0x00, 0x00, 0x00, 0x17, 0x10, 0x00, 0x00, 0x01, 0x00, 0x16, 0x16,
+	0xf4,	15, 0x00, 0x09, 0x00, 0x00, 0x00, 0x3f, 0x3f, 0x07, 0x00, 0x3C, 0x36, 0x00, 0x3C, 0x36, 0x00,
+	0x36,	1,	0x08, 
+	0x35,	1, 0x00,
+	0x3a,	1+DELAY, 0x05, 150,
+	0x29,	0,
+	0x2c,	0 };
+
+
+// Companion code to the above tables.  Reads and issues
+// a series of LCD commands stored in PROGMEM byte array.
+void Adafruit_QDTech::commandList(const uint8_t *addr) {
+
+  uint8_t  numCommands, numArgs;
+  uint16_t ms;
+
+  numCommands = pgm_read_byte(addr++);   // Number of commands to follow
+  
+  while(numCommands--) {                 // For each command...
+	writecommand(pgm_read_byte(addr++)); //   Read, issue command
+    numArgs  = pgm_read_byte(addr++);    //   Number of args to follow
+    ms       = numArgs & DELAY;          //   If hibit set, delay follows args
+    numArgs &= ~DELAY;                   //   Mask out delay bit
+    while(numArgs--) {                   //   For each argument...
+      writedata(pgm_read_byte(addr++));  //     Read, issue argument
+    }
+    if(ms) {
+      ms = pgm_read_byte(addr++); // Read post-command delay time (ms)
+      if(ms == 255) ms = 500;     // If 255, delay for 500 ms
+      delay(ms);
+    }
+  }
+}
+
+
+// Initialization code for  QDTech displays
+void Adafruit_QDTech::commonInit(const uint8_t *cmdList) {
+  colstart  = rowstart = 0; // May be overridden in init func
+
+  pinMode(_rs, OUTPUT);
+  pinMode(_cs, OUTPUT);
+  csport    = portOutputRegister(digitalPinToPort(_cs));
+  rsport    = portOutputRegister(digitalPinToPort(_rs));
+  cspinmask = digitalPinToBitMask(_cs);
+  rspinmask = digitalPinToBitMask(_rs);
+
+  if(hwSPI) { // Using hardware SPI
+    SPI.begin();
+    SPI.setClockDivider(SPI_CLOCK_DIV4); // 4 MHz (half speed)
+    //Due defaults to 4mHz (clock divider setting of 21)
+    SPI.setBitOrder(MSBFIRST);
+    SPI.setDataMode(SPI_MODE0);
+  } else {
+    pinMode(_sclk, OUTPUT);
+    pinMode(_sid , OUTPUT);
+    clkport     = portOutputRegister(digitalPinToPort(_sclk));
+    dataport    = portOutputRegister(digitalPinToPort(_sid));
+    clkpinmask  = digitalPinToBitMask(_sclk);
+    datapinmask = digitalPinToBitMask(_sid);
+    *clkport   &= ~clkpinmask;
+    *dataport  &= ~datapinmask;
+  }
+
+  // toggle RST low to reset; CS low so it'll listen to us
+  *csport &= ~cspinmask;
+  if (_rst) {
+    pinMode(_rst, OUTPUT);
+    digitalWrite(_rst, HIGH);
+    delay(500);
+    digitalWrite(_rst, LOW);
+    delay(500);
+    digitalWrite(_rst, HIGH);
+    delay(500);
+  }
+
+  if(cmdList) commandList(cmdList);
+}
+
+
+// Initialization for QDTech LCD only
+void Adafruit_QDTech::init(uint8_t options) {
+	commonInit(0);
+	commandList(QDTech);
+	tabcolor = options;
+}
+
+
+void Adafruit_QDTech::setAddrWindow(uint8_t x0, uint8_t y0, uint8_t x1,
+ uint8_t y1) {
+
+  writecommand(QDTech_CASET); // Column addr set
+  writedata(0x00);
+  writedata(x0+colstart);     // XSTART 
+  writedata(0x00);
+  writedata(x1+colstart);     // XEND
+
+  writecommand(QDTech_RASET); // Row addr set
+  writedata(0x00);
+  writedata(y0+rowstart);     // YSTART
+  writedata(0x00);
+  writedata(y1+rowstart);     // YEND
+
+  writecommand(QDTech_RAMWR); // write to RAM
+}
+
+
+void Adafruit_QDTech::pushColor(uint16_t color) {
+  *rsport |=  rspinmask;
+  *csport &= ~cspinmask;
+
+  spiwrite(color >> 8);
+  spiwrite(color);
+
+  *csport |= cspinmask;
+ }
+
+void Adafruit_QDTech::drawPixel(int16_t x, int16_t y, uint16_t color) {
+
+  if((x < 0) ||(x >= _width) || (y < 0) || (y >= _height)) return;
+
+  setAddrWindow(x,y,x+1,y+1);
+
+  *rsport |=  rspinmask;
+  *csport &= ~cspinmask;
+  
+  spiwrite(color >> 8);
+  spiwrite(color);
+
+  *csport |= cspinmask;
+}
+
+void Adafruit_QDTech::drawFastVLine(int16_t x, int16_t y, int16_t h,
+ uint16_t color) {
+
+  // Rudimentary clipping
+  if((x >= _width) || (y >= _height)) return;
+  if((y+h-1) >= _height) h = _height-y;
+  setAddrWindow(x, y, x, y+h-1);
+
+  uint8_t hi = color >> 8, lo = color;
+  *rsport |=  rspinmask;
+  *csport &= ~cspinmask;
+  while (h--) {
+    spiwrite(hi);
+    spiwrite(lo);
+  }
+  *csport |= cspinmask;
+}
+
+
+void Adafruit_QDTech::drawFastHLine(int16_t x, int16_t y, int16_t w,
+  uint16_t color) {
+
+  // Rudimentary clipping
+  if((x >= _width) || (y >= _height)) return;
+  if((x+w-1) >= _width)  w = _width-x;
+  setAddrWindow(x, y, x+w-1, y);
+
+  uint8_t hi = color >> 8, lo = color;
+  *rsport |=  rspinmask;
+  *csport &= ~cspinmask;
+  while (w--) {
+    spiwrite(hi);
+    spiwrite(lo);
+  }
+  *csport |= cspinmask;
+}
+
+
+
+void Adafruit_QDTech::fillScreen(uint16_t color) {
+  fillRect(0, 0,  _width, _height, color);
+}
+
+
+
+// fill a rectangle
+void Adafruit_QDTech::fillRect(int16_t x, int16_t y, int16_t w, int16_t h,
+  uint16_t color) {
+
+  // rudimentary clipping (drawChar w/big text requires this)
+  if((x >= _width) || (y >= _height)) return;
+  if((x + w - 1) >= _width)  w = _width  - x;
+  if((y + h - 1) >= _height) h = _height - y;
+
+  setAddrWindow(x, y, x+w-1, y+h-1);
+
+  uint8_t hi = color >> 8, lo = color;
+  *rsport |=  rspinmask;
+  *csport &= ~cspinmask;
+  for(y=h; y>0; y--) {
+    for(x=w; x>0; x--) {
+      spiwrite(hi);
+      spiwrite(lo);
+    }
+  }
+  *csport |= cspinmask;
+}
+
+// Pass 8-bit (each) R,G,B, get back 16-bit packed color
+uint16_t Adafruit_QDTech::Color565(uint8_t r, uint8_t g, uint8_t b) {
+// Packing order for QDTech is BGR, not RGB
+  return ((b & 0xF8) << 8) | ((g & 0xFC) << 3) | (r >> 3);
+}
+
+
+#define MADCTL_MY  0x80
+#define MADCTL_MX  0x40
+#define MADCTL_MV  0x20
+#define MADCTL_ML  0x10
+#define MADCTL_RGB 0x00
+#define MADCTL_BGR 0x08
+#define MADCTL_MH  0x04
+
+void Adafruit_QDTech::setRotation(uint8_t m) {
+// Generally 0 - Portrait 1 - Landscape
+
+  writecommand(QDTech_MADCTL);
+  rotation = m % 4; // can't be higher than 3
+  switch (rotation) {
+   case 0:
+     if (tabcolor == 0x0) {
+       writedata(MADCTL_MX | MADCTL_MY | MADCTL_RGB);
+     } else {
+       writedata(MADCTL_MX | MADCTL_MY | MADCTL_BGR);
+     }
+     _width  = QDTech_TFTWIDTH;
+     _height = QDTech_TFTHEIGHT;
+     break;
+   case 1:
+     if (tabcolor == 0x0) {
+       writedata(MADCTL_MY | MADCTL_MV | MADCTL_RGB);
+     } else {
+       writedata(MADCTL_MY | MADCTL_MV | MADCTL_BGR);
+     }
+     _width  = QDTech_TFTHEIGHT;
+     _height = QDTech_TFTWIDTH;
+     break;
+  case 2:
+     if (tabcolor == 0x0) {
+       writedata(MADCTL_RGB);
+     } else {
+       writedata(MADCTL_BGR);
+     }
+     _width  = QDTech_TFTWIDTH;
+     _height = QDTech_TFTHEIGHT;
+    break;
+   case 3:
+     if (tabcolor == 0x0) {
+       writedata(MADCTL_MX | MADCTL_MV | MADCTL_RGB);
+     } else {
+       writedata(MADCTL_MX | MADCTL_MV | MADCTL_BGR);
+     }
+     _width  = QDTech_TFTHEIGHT;
+     _height = QDTech_TFTWIDTH;
+     break;
+  }
+}
+
+
+void Adafruit_QDTech::invertDisplay(boolean i) {
+  writecommand(i ? QDTech_INVON : QDTech_INVOFF);
+}
